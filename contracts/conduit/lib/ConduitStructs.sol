5c4f66e87df128c1c5a26965e235404c3e857051
www <103179223+www2022@users.noreply.github.com>
2023-01-28 16:51:03 +0800

    Update ConduitStructs.sol

--- contracts/conduit/lib/ConduitStructs.sol
+++ contracts/conduit/lib/ConduitStructs.sol
@@ -1,26 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
 import { ConduitItemType } from "./ConduitEnums.sol";
 
-/**
- * @dev A ConduitTransfer is a struct that contains the information needed for a
- *      conduit to transfer an item from one address to another.
- */
 struct ConduitTransfer {
     ConduitItemType itemType;
     address token;
     address from;
     address to;
-    uint256 identifier;
+    uint256 identifier; //根据itemType类型，含义不同：对于erc721、1155，identifier为确定的一个 tokenId；如果itemType是 ..._WITH_CRITERIA,identifier为Merkle Root，具体的tokenId由交易入参中的struct CriteriaResolver指定
     uint256 amount;
 }
 
-/**
- * @dev A ConduitBatch1155Transfer is a struct that contains the information
- *      needed for a conduit to transfer a batch of ERC-1155 tokens from one
- *      address to another.
- */
 struct ConduitBatch1155Transfer {
     address token;
     address from;
